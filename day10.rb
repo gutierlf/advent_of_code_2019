2528dbbc47cc6ca9200441926c2f65d15edfea3c
Luis F Gutierrez <luis@gutierrezhiller.com>
2019-12-12 00:51:07 -0800

    day 10 step 2

--- day10.rb
+++ day10.rb
@@ -1,7 +1,25 @@
 require "set"
 
-def monitoring_station_location_for(map)
-  asteroid_pairs = asteroids(map).permutation(2).to_a
+def vaporized_asteroids(monitor, asteroids)
+  asteroids_by_direction = asteroids
+    .map { |asteroid| Vector.new(monitor, asteroid) }
+    .group_by { |vector| vector.clockwise_angle }
+    .to_a
+    .sort { |a, b| a[0] <=> b[0] }
+    .map { |angle, list| [angle, list.sort_by { |vector| vector.magnitude_squared }]}
+
+  vaporized = []
+  i = 0
+  while asteroids_by_direction.flatten.any? { |el| el.is_a?(Vector) }
+    _, list_in_direction = asteroids_by_direction[i % asteroids_by_direction.length]
+    vaporized << list_in_direction.shift unless list_in_direction.empty?
+    i += 1
+  end
+  vaporized.map { |vector| vector.to }
+end
+
+def monitoring_station_location_for(asteroids)
+  asteroid_pairs = asteroids.permutation(2).to_a
   asteroid_to_directions = asteroid_pairs.reduce({}) do |acc, (from, to)|
     acc[from] = acc.fetch(from, Set.new) << Vector.new(from, to).normalized
     acc
@@ -26,6 +44,14 @@ end
 Point = Struct.new(:col, :row)
 
 Vector = Struct.new(:from, :to) do
+  def magnitude_squared
+    u**2 + v**2
+  end
+
+  def clockwise_angle
+    Math.atan2(u, -v) % (2 * Math::PI)
+  end
+
   def u
     to_minus_from(:col)
   end
@@ -47,7 +73,7 @@ end
 
 NormalizedVector = Struct.new(:i, :j) do
   def self.for(vec)
-    mag_squared = vec.u**2 + vec.v**2
+    mag_squared = vec.magnitude_squared
     u_gcd = vec.u.gcd(mag_squared)
     v_gcd = vec.v.gcd(mag_squared)
     denominator = u_gcd.gcd(v_gcd)
@@ -57,6 +83,12 @@ end
 
 if __FILE__ == $0
   map = File.read("day10_input.txt")
-  answer1 = monitoring_station_location_for(map)
+  asteroids = asteroids(map)
+  monitor, answer1 = monitoring_station_location_for(asteroids)
   puts answer1
+
+  vaporized = vaporized_asteroids(monitor, asteroids - [monitor])
+  vaporized200 = vaporized[199]
+  answer2 = vaporized200.col * 100 + vaporized200.row
+  puts answer2
 end
--- day10_spec.rb
+++ day10_spec.rb
@@ -19,89 +19,148 @@ RSpec.describe "day 10" do
       expect(Vector.new(Point.new(0, 0), Point.new(2, 2)).normalized).to eq NormalizedVector.new(1, 1)
     end
 
-    it "example 1" do
-      map = <<-MAP
-.#..#
-.....
-#####
-....#
-...##
-      MAP
-      expect(monitoring_station_location_for(map)[0]).to eq Point.new(3, 4)
+    shared_examples "monitoring_station_location_for" do |expected|
+      it "works" do
+        asteroids = asteroids(map)
+        expect(monitoring_station_location_for(asteroids)[0]).to eq expected
+      end
     end
 
-    it "example 2" do
-      map = <<~MAP
-......#.#.
-#..#.#....
-..#######.
-.#.#.###..
-.#..#.....
-..#....#.#
-#..#....#.
-.##.#..###
-##...#..#.
-.#....####
-      MAP
-      expect(monitoring_station_location_for(map)[0]).to eq Point.new(5, 8)
+    context "example 1" do
+      let(:map) do
+        <<~MAP
+          .#..#
+          .....
+          #####
+          ....#
+          ...##
+        MAP
+      end
+      include_examples "monitoring_station_location_for", Point.new(3, 4)
     end
 
-    it "example 3" do
-      map = <<~MAP
-#.#...#.#.
-.###....#.
-.#....#...
-##.#.#.#.#
-....#.#.#.
-.##..###.#
-..#...##..
-..##....##
-......#...
-.####.###.
-      MAP
-      expect(monitoring_station_location_for(map)[0]).to eq Point.new(1, 2)
+    context "example 2" do
+      let(:map) do
+        <<~MAP
+          ......#.#.
+          #..#.#....
+          ..#######.
+          .#.#.###..
+          .#..#.....
+          ..#....#.#
+          #..#....#.
+          .##.#..###
+          ##...#..#.
+          .#....####
+        MAP
+      end
+      include_examples "monitoring_station_location_for", Point.new(5, 8)
     end
 
-    it "example 4" do
-      map = <<~MAP
-.#..#..###
-####.###.#
-....###.#.
-..###.##.#
-##.##.#.#.
-....###..#
-..#.#..#.#
-#..#.#.###
-.##...##.#
-.....#.#..
-      MAP
-      expect(monitoring_station_location_for(map)[0]).to eq Point.new(6, 3)
+    context "example 3" do
+      let(:map) do
+        <<~MAP
+          #.#...#.#.
+          .###....#.
+          .#....#...
+          ##.#.#.#.#
+          ....#.#.#.
+          .##..###.#
+          ..#...##..
+          ..##....##
+          ......#...
+          .####.###.
+        MAP
+      end
+      include_examples "monitoring_station_location_for", Point.new(1, 2)
     end
 
-    it "example 5" do
+    context "example 4" do
+      let(:map) do
+        <<~MAP
+          .#..#..###
+          ####.###.#
+          ....###.#.
+          ..###.##.#
+          ##.##.#.#.
+          ....###..#
+          ..#.#..#.#
+          #..#.#.###
+          .##...##.#
+          .....#.#..
+        MAP
+      end
+      include_examples "monitoring_station_location_for", Point.new(6, 3)
+    end
+
+    context "example 5" do
+      let(:map) do
+        <<~MAP
+          .#..##.###...#######
+          ##.############..##.
+          .#.######.########.#
+          .###.#######.####.#.
+          #####.##.#.##.###.##
+          ..#####..#.#########
+          ####################
+          #.####....###.#.#.##
+          ##.#################
+          #####.##.###..####..
+          ..######..##.#######
+          ####.##.####...##..#
+          .#####..#.######.###
+          ##...#.##########...
+          #.##########.#######
+          .####.#.###.###.#.##
+          ....##.##.###..#####
+          .#.#.###########.###
+          #.#.#.#####.####.###
+          ###.##.####.##.#..##
+        MAP
+      end
+      include_examples "monitoring_station_location_for", Point.new(11, 13)
+    end
+  end
+
+  describe "step 2" do
+    it "example" do
       map = <<~MAP
-.#..##.###...#######
-##.############..##.
-.#.######.########.#
-.###.#######.####.#.
-#####.##.#.##.###.##
-..#####..#.#########
-####################
-#.####....###.#.#.##
-##.#################
-#####.##.###..####..
-..######..##.#######
-####.##.####...##..#
-.#####..#.######.###
-##...#.##########...
-#.##########.#######
-.####.#.###.###.#.##
-....##.##.###..#####
-.#.#.###########.###
-#.#.#.#####.####.###
-###.##.####.##.#..##
+        .#..##.###...#######
+        ##.############..##.
+        .#.######.########.#
+        .###.#######.####.#.
+        #####.##.#.##.###.##
+        ..#####..#.#########
+        ####################
+        #.####....###.#.#.##
+        ##.#################
+        #####.##.###..####..
+        ..######..##.#######
+        ####.##.####...##..#
+        .#####..#.######.###
+        ##...#.##########...
+        #.##########.#######
+        .####.#.###.###.#.##
+        ....##.##.###..#####
+        .#.#.###########.###
+        #.#.#.#####.####.###
+        ###.##.####.##.#..##
       MAP
-      expect(monitoring_station_location_for(map)[0]).to eq Point.new(11, 13)
+      asteroids = asteroids(map)
+      monitor, _ = monitoring_station_location_for(asteroids)
+      list = vaporized_asteroids(monitor, asteroids - [monitor])
+      expect(list.length).to eq 299
+      expect(list[0]).to eq Point.new(11, 12)
+      expect(list[1]).to eq Point.new(12, 1)
+      expect(list[2]).to eq Point.new(12, 2)
+      expect(list[9]).to eq Point.new(12, 8)
+      expect(list[19]).to eq Point.new(16, 0)
+      expect(list[49]).to eq Point.new(16, 9)
+      expect(list[99]).to eq Point.new(10, 16)
+      expect(list[198]).to eq Point.new(9, 6)
+      expect(list[199]).to eq Point.new(8, 2)
+      expect(list[200]).to eq Point.new(10, 9)
+      expect(list[298]).to eq Point.new(11, 1)
     end
   end
 end
